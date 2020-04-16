94690b25bdcd385e9668dda87329cb2f668f58c1
codeseele <673608135@qq.com>
2020-04-16 22:34:13 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+Vue.use(ElementUI)
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+
 Vue.config.productionTip = false
 
 new Vue({
