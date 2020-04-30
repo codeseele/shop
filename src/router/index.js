7effab58bfbe8ac30ce8958954eff560fcc3e805
codeseele <673608135@qq.com>
2020-04-30 23:25:18 +0800

    完成报表

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ const Params = () => import('../components/goods/Params')
 const List = () => import('../components/goods/List')
 const Add = () => import('../components/goods/Add')
 const Order = () => import('../components/order/Order')
+const Report = () => import('../components/report/Report')
 
 
 Vue.use(VueRouter)
@@ -65,6 +66,10 @@ const routes = [
       {
         path: '/orders',
         component: Order
+      },
+      {
+        path: '/reports',
+        component: Report
       }
     ]
   },
