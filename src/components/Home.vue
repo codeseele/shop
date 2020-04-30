a18142d9b17343a0c643228c4dcb97f8a345a1c0
codeseele <673608135@qq.com>
2020-04-30 17:04:06 +0800

    完成商品功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,7 +15,7 @@
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
         <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF"
-         unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="$route.path">
+         unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单模板区域 -->
@@ -24,7 +24,7 @@
               <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单区域 -->
-            <el-menu-item :index="'/' + subItem.path " v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item :index="'/' + subItem.path " v-for="subItem in item.children" :key="subItem.id" @click="savaNavState('/' + subItem.path)">
 
               <template slot="title">
                 <!-- 图标 -->
@@ -61,11 +61,14 @@ export default {
         '145': 'iconfont iconbaobiao'
       },
       // 是否折叠
-      isCollapse: false
+      isCollapse: false,
+// 被激活的链接地址
+      activePath: ''
     }
   },
   created() {
     this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
 
   methods: {
@@ -84,7 +87,11 @@ export default {
       this.menulist = res.data
       console.log(res);
     },
-
+    // 保存链接的激活状态
+    savaNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+    }
   }
 }
 </script>
